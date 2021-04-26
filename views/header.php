838d74918ffa06c170e1b758ac99a7d6896329a2
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-26 20:00:59 -0300

    Update header.php

--- views/header.php
+++ views/header.php
@@ -8,25 +8,41 @@
 </head>
 <body>
     <header>
+<!--NAVBAR-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!--CONTAINER-->
             <div class="container-fluid">
+                <!--Título-->
                 <a class="navbar-brand" href="#" id="logo_string">Tricotando amor</a>
+                <!---------->
+                <!--Botão sanduíche-->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav" style="padding: 10px;">
-                    <ul class="navbar-nav" id="menu_list" style="padding: 10px;">
+                <!------------------>
+                <!--Menu com opções de seleção (Com colapso caso a tela diminua demais, 
+                caso entre em colapso, o BOTÃO SANDUÍCHE é chamado)-->
+                <div class="collapse navbar-collapse" id="navbarNav" style="margin-left: 700px;">
+                    <!--Lista não ordenada-->
+                    <ul class="navbar-nav" id="menu_list" style="padding: 10px; float:right">
+                        <!--HOME-->
                         <li class="nav-item link_menu">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
+                        <!--QUEM SOMOS-->
                         <li class="nav-item link_menu">
                         <a class="nav-link" href="#">Quem somos</a>
                         </li>
+                        <!--FALE CONOSCO-->
                         <li class="nav-item link_menu">
                         <a class="nav-link" href="#">Fale conosco</a>
                         </li>
                     </ul>
                 </div>
+                <!------------------------------------>
             </div>
+            <!--/CONTAINER-->
         </nav>
+<!--/NAVBAR-->
     </header>
+<!--- FINAL DA HEADER --->
